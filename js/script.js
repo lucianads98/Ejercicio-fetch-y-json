0d043ff72d04f0462f2ec84903d963b7f414f957
Luciana De Souza <129917797+lucianads98@users.noreply.github.com>
2024-08-30 18:49:09 -0300

    Array

--- js/script.js
+++ js/script.js
@@ -12,10 +12,28 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  */
 function showData(dataArray) {
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
+  for (const item of dataArray.students) {
+    console.log("nombre :" +item.name +" "+ item.lastname)
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
     container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+function loadData() {
+  fetch(DATA_URL)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          showData(data);
+      })
+      .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+      });
+}
+
+document.addEventListener('DOMContentLoaded', loadData);
